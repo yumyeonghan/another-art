9a321d1404c032346ed7869fc9a27c62cadacd15
eunchannam <75837025+eunchannam@users.noreply.github.com>
2022-08-26 20:22:14 +0900

    [BE] 회원가입 예외 처리 기능 | AA-76-회원가입-예외-처리 => BE (#14)
    * AA-76 로그인 아이디 중복 예외 처리
    
    * AA-76 닉네임 중복 예외 처리
    
    * AA-76 전화번호 중복 예외 처리 + 버그 수정
    
    * 중복체크 컨트롤러 분리
    
    * 중복예외 응답 상태코드 수정(406 -> 409)

--- src/main/java/com/imagine/another_arts/web/user/UserController.java
+++ src/main/java/com/imagine/another_arts/web/user/UserController.java
@@ -5,6 +5,8 @@ import com.imagine.another_arts.domain.point.repository.PointHistoryRepository;
 import com.imagine.another_arts.domain.user.Users;
 import com.imagine.another_arts.domain.user.repository.UserRepository;
 import com.imagine.another_arts.domain.user.service.UserService;
+import com.imagine.another_arts.exception.IllegalUrlException;
+import com.imagine.another_arts.exception.UserDuplicationException;
 import com.imagine.another_arts.web.SimpleSucessResponse;
 import com.imagine.another_arts.web.user.dto.UserEditRequest;
 import com.imagine.another_arts.web.user.dto.UserJoinRequest;
@@ -54,5 +56,37 @@ public class UserController {
     ) {
         userService.editUser(userId, editForm);
         return new SimpleSucessResponse(true);
+    }
+
+    @GetMapping("/user/loginId/duplication/{target}")
+    @ApiOperation(value = "로그인 아이디 중복체크 API", notes = "중복체크 할 데이터를 서버에 전달함에 따라 사용 가능한 데이터인지 여부를 반환")
+    public SimpleSucessResponse loginIdDuplicationCheck(@PathVariable String target) {
+        boolean present = userRepository.findFirstByLoginId(target).isPresent();
+        throwError(present, "이미 존재하는 로그인 아이디 입니다.");
+
+        return new SimpleSucessResponse(true);
+    }
+
+    @GetMapping("/user/nickname/duplication/{target}")
+    @ApiOperation(value = "닉네임 중복체크 API", notes = "중복체크 할 데이터를 서버에 전달함에 따라 사용 가능한 데이터인지 여부를 반환")
+    public SimpleSucessResponse nicknameDuplicationCheck(@PathVariable String target) {
+        boolean present = userRepository.findFirstByNickname(target).isPresent();
+        throwError(present, "이미 존재하는 닉네임 입니다.");
+
+        return new SimpleSucessResponse(true);
+    }
+
+    @GetMapping("/user/phoneNumber/duplication/{target}")
+    @ApiOperation(value = "전화번호 중복체크 API", notes = "중복체크 할 데이터를 서버에 전달함에 따라 사용 가능한 데이터인지 여부를 반환")
+    public SimpleSucessResponse phoneNumberDuplicationCheck(@PathVariable String target) {
+        boolean present = userRepository.findFirstByPhoneNumber(target).isPresent();
+        throwError(present, "이미 존재하는 전화번호 입니다.");
+
+        return new SimpleSucessResponse(true);
+    }
+
+    // 중복 여부에 따라 중복에러를 던짐
+    private void throwError(boolean present, String message) {
+        if (present) throw new UserDuplicationException(message);
     }
 }
