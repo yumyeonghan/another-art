79060a48ab3d5456aaf4531e9c231e252cb5434c
yumyeonghan <mgtmh991013@gmail.com>
2022-09-07 19:03:52 +0900

    AA-79 포인트 충전

---
+++ src/main/java/com/imagine/another_arts/web/point/PointExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.imagine.another_arts.web.point;
+
+import com.imagine.another_arts.exception.ErrorDescription;
+import com.imagine.another_arts.exception.IllegalUserInfoFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice("com.imagine.another_arts.web.point")
+public class PointExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalUserInfoFoundException.class)
+    public ErrorDescription illegalUserInfoFoundException(IllegalUserInfoFoundException e) {
+        return new ErrorDescription(
+                false,
+                "BAD_REQUEST",
+                e.getMessage()
+        );
+    }
+}
