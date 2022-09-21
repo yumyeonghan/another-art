f53c642cd8470cbec372f7fd329a26ba235c6f0d
SeoJiWon <sjiwon4491@gmail.com>
2022-09-21 18:51:11 +0900

    AA-85 회원가입 중복체크 API 구현

--- src/main/java/com/imagine/another_arts/web/user/UserController.java
+++ src/main/java/com/imagine/another_arts/web/user/UserController.java
@@ -1,6 +1,8 @@
 package com.imagine.another_arts.web.user;
 
 import com.imagine.another_arts.domain.user.service.UserService;
+import com.imagine.another_arts.exception.IllegalUrlRequestException;
+import com.imagine.another_arts.web.user.dto.request.DuplicateCheckRequest;
 import com.imagine.another_arts.web.user.dto.request.UserEditRequest;
 import com.imagine.another_arts.web.user.dto.request.UserJoinRequest;
 import com.imagine.another_arts.web.user.dto.response.SimpleUserSuccessResponse;
@@ -37,5 +39,33 @@ public class UserController {
     public ResponseEntity<Void> editUser(@PathVariable Long userId, @RequestBody UserEditRequest userEditRequest) {
         userService.editUser(userId, userEditRequest.toServiceDto());
         return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/user/duplicate-check")
+    @ApiOperation(value = "회원가입 중복 검사 API", notes = "[닉네임, 로그인 아이디, 전화번호, 이메일]에 대한 중복 검사 API")
+    public ResponseEntity<Void> validateInformationInSignUp(@Valid @RequestBody DuplicateCheckRequest duplicateCheckRequest) {
+        String resource = duplicateCheckRequest.getResource();
+        String input = duplicateCheckRequest.getInput();
+
+        switch (resource) {
+            case "nickname":
+                userService.hasDuplicateNickname(input);
+                return ResponseEntity.noContent().build();
+
+            case "loginId":
+                userService.hasDuplicateLoginId(input);
+                return ResponseEntity.noContent().build();
+
+            case "phoneNumber":
+                userService.hasDuplicatePhoneNumber(input);
+                return ResponseEntity.noContent().build();
+
+            case "email":
+                userService.hasDuplicateEmail(input);
+                return ResponseEntity.noContent().build();
+
+            default:
+                throw new IllegalUrlRequestException("잘못된 요청입니다");
+        }
     }
 }
