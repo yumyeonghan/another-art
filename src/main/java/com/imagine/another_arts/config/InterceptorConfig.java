2c96743bc306f93fcfbe409ea299cc9658967616
EunChanNam <75837025+EunChanNam@users.noreply.github.com>
2022-09-26 13:42:43 +0900

    [BE] AA-86 로그인 인터셉터 구현 (#29)
    * 인터셉터 구현 + 인서셉터 등록
    
    * 로그인, 회원가입 경로 인터셉터 적용 제외

---
+++ src/main/java/com/imagine/another_arts/config/InterceptorConfig.java
@@ -0,0 +1,19 @@
+package com.imagine.another_arts.config;
+
+import com.imagine.another_arts.web.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        WebMvcConfigurer.super.addInterceptors(registry);
+        registry.addInterceptor(new LoginInterceptor())
+                .order(1)
+                .addPathPatterns("/api/**")
+                .excludePathPatterns("/api/user", "/api/login");
+    }
+}
