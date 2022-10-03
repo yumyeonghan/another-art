f6584547d86dce1c8e68b6c547989f597913b1d7
SeoJiWon <sjiwon4491@gmail.com>
2022-10-03 19:47:24 +0900

    [Fix] Exception Enum StatusCode 수정

--- src/main/java/com/imagine/another_arts/exception/AnotherArtErrorCode.java
+++ src/main/java/com/imagine/another_arts/exception/AnotherArtErrorCode.java
@@ -8,13 +8,13 @@ public enum AnotherArtErrorCode {
     // User
     AUTHENTICATION_USER(HttpStatus.UNAUTHORIZED, "로그인하지 않은 사용자입니다"),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자 정보가 존재하지 않습니다"),
-    WRONG_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다"),
+    WRONG_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다"),
     DUPLICATE_USER_EMAIL(HttpStatus.CONFLICT, "중복된 이메일입니다"),
     DUPLICATE_USER_LOGIN_ID(HttpStatus.CONFLICT, "중복된 로그인 아이디입니다"),
     DUPLICATE_USER_NICKNAME(HttpStatus.CONFLICT, "중복된 닉네임입니다"),
     DUPLICATE_USER_PHONENUMBER(HttpStatus.CONFLICT, "중복된 전화번호입니다"),
     ILLEGAL_USER_API_REQUEST(HttpStatus.BAD_REQUEST, "타인의 정보는 요청할 수 없습니다"),
-    INELIGIBLE_USER_PURCHASE(HttpStatus.BAD_REQUEST, "구매 자격이 없는 사용자입니다"),
+    INELIGIBLE_USER_PURCHASE(HttpStatus.FORBIDDEN, "구매 자격이 없는 사용자입니다"),
 
     // Art
     ART_NOT_FOUND(HttpStatus.NOT_FOUND, "작품 정보가 존재하지 않습니다"),
@@ -33,7 +33,7 @@ public enum AnotherArtErrorCode {
     NOT_CLOSED_AUCTION(HttpStatus.BAD_REQUEST, "종료되지 않은 경매입니다"),
 
     // Point
-    POINT_NOT_ENOUGH(HttpStatus.BAD_REQUEST, "포인트가 부족합니다"),
+    POINT_NOT_ENOUGH(HttpStatus.CONFLICT, "포인트가 부족합니다"),
 
     // API Request
     ILLEGAL_URL_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다"),
