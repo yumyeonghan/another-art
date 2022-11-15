b4c9790deedad4364c69559abe9d0712fba7bb23
jae woo Jung <awfjol2008@naver.com>
2022-11-15 23:20:47 +0900

    [Fix] VueController 수정

--- src/main/java/com/imagine/another_arts/web/VueController.java
+++ src/main/java/com/imagine/another_arts/web/VueController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VueController {
-    @GetMapping("/vue")
+    @GetMapping("/")
     public String main() {
         return "vue/index";
     }
